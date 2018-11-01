ca31bfc3239af6e419a397bf5f4058e8f1b3347a
Simon Rowe <simon.rowe@gmail.com>
2018-11-01 11:33:18 +0000

    clean up of TokenController

--- src/main/java/io/pivotal/concoursepushbutton/TokenController.java
+++ src/main/java/io/pivotal/concoursepushbutton/TokenController.java
@@ -2,12 +2,14 @@ package io.pivotal.concoursepushbutton;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletResponse;
 
-@RestController
+@Controller
 @RequestMapping("/token")
 public class TokenController {
 
@@ -21,19 +23,14 @@ public class TokenController {
     private String redirectUrl;
 
 
-    @GetMapping("display")
-    public String display() {
-        return tokenService.getBearerToken();
-    }
-
     @GetMapping("get")
-    public void getBearerToken(HttpServletResponse response) throws Exception {
-        response.sendRedirect(concourseLoginUrl + redirectUrl);
+    public String getBearerToken() {
+        return "redirect:" + concourseLoginUrl + redirectUrl;
     }
 
     @RequestMapping("set")
-    public ResponseEntity setBearerToken(@RequestParam("token") String token) {
+    public String setBearerToken(@RequestParam("token") String token, HttpServletResponse response) {
         tokenService.setBearerToken(token);
-        return ResponseEntity.ok().build();
+        return "redirect:/";
     }
 }
