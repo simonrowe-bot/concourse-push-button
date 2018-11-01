be737325f81c0c9147fe1905e5c39493ce267025
Simon Rowe <simon.rowe@gmail.com>
2018-11-01 12:24:36 +0000

    Add in home controller

--- src/main/java/io/pivotal/concoursepushbutton/ConcoursePushButtonApplication.java
+++ src/main/java/io/pivotal/concoursepushbutton/ConcoursePushButtonApplication.java
@@ -5,7 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableWebMvc
 @SpringBootApplication
 public class ConcoursePushButtonApplication {
 
@@ -17,4 +21,6 @@ public class ConcoursePushButtonApplication {
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 		return builder.build();
 	}
+
+
 }
